d6f25a7ceb198065963ec2e8c44a022301cd4eee
minchanhan <minchanhan5@gmail.com>
2020-04-18 23:11:45 -0400

    Added iteration of cards using Handlebars

---
+++ index.js
@@ -0,0 +1,68 @@
+let cpp = [
+    {
+        id: 1,
+        name: "Web 1",
+        link: "link",
+        language: "C++",
+        description:
+            "Helpful diagrams, great feedback",
+        image_link: "res/preview_pic.png",
+        rating: "5"
+    },
+    {
+        id: 2,
+        name: "Web 2",
+        link: "link",
+        language: "C++",
+        description:
+            "Helpful diagrams, great feedback",
+        image_link: "res/preview_pic.png",
+        rating: "3"
+    },
+    {
+        id: 3,
+        name: "Web 3",
+        link: "link",
+        language: "C++",
+        description:
+            "Helpful diagrams, great feedback",
+        image_link: "res/preview_pic.png",
+        rating: "2"
+    }
+];
+
+let python = [
+    {
+        id: 1,
+        name: "Web 1",
+        link: "link",
+        language: "Python",
+        description:
+            "Helpful diagrams, great feedback",
+        image_link: "res/preview_pic.png",
+        rating: "5"
+    },
+    {
+        id: 2,
+        name: "Web 2",
+        link: "link",
+        language: "python",
+        description:
+            "Helpful diagrams, great feedback",
+        image_link: "res/preview_pic.png",
+        rating: "4"
+    }
+];
+
+let languagecards = [["C++", cpp], ["Python", python]];
+
+const replaceHTMLlanguagecards = () => {
+    var source = document.getElementById("languagecardtemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    var context = { languages: languagecards };
+    var html = template(context);
+    document.getElementById("languagecards").innerHTML = html;
+};
+  
+replaceHTMLlanguagecards();
+
